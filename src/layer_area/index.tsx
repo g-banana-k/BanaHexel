38ea876849729435c28118d5a2578cc88ad249d3
bananahexagon <obsidian.bananafool@gmail.com>
2024-10-06 22:34:12 +0900

    #

--- src/layer_area/index.tsx
+++ src/layer_area/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from "react";
 import "./index.css";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { canvas_size_state, current_layer_state, layer_arr_state } from "../app";
-import { Plus, X } from "lucide-react";
+import { MoveDown, MoveUp, Plus, X } from "lucide-react";
 import { Layer } from "../data";
 
 export const LayerArea = () => {
@@ -15,7 +15,7 @@ export const LayerArea = () => {
         layer_arr?.forEach((l, i) => {
             const preview_div = div_refs.current[i];
             if (!preview_div) return;
-            preview_div.innerHTML="";
+            preview_div.innerHTML = "";
 
             l.preview.classList.add("layer_thumbnail_preview");
             preview_div.appendChild(l.preview);
@@ -40,17 +40,42 @@ export const LayerArea = () => {
                         />
                         <div
                             className="layer_thumbnail_delete_button"
-                            onClick={
-                                () => {
-                                    set_layer_arr(_ => _!.filter((_, j) => j !== i))
-                                    if (layer_arr.length - 1 <= current_layer) set_current_layer(_ => _ - 1);
-                                }
-                            }
+                            onClick={() => {
+                                set_layer_arr(_ => _!.filter((_, j) => j !== i))
+                                if (layer_arr.length - 1 <= current_layer) set_current_layer(_ => _ - 1);
+                            }}
                             style={{
                                 display: layer_arr.length !== 1 ? "block" : "none",
                             }}
                         >
                             <X size={16} />
+                        </div>
+                        <div
+                            className="layer_thumbnail_move_button_area"
+                            style={{
+                                display: layer_arr.length !== 1 ? "block" : "none",
+                            }}
+                        >
+                            <div className="layer_thumbnail_move_button" onClick={() => {
+                                if (i === 0) return;
+                                const l = [...layer_arr];
+                                if (i === current_layer) { set_current_layer(i - 1); }
+                                else if (i - 1 === current_layer) { set_current_layer(i); }
+                                [l[i], l[i - 1]] = [l[i - 1], l[i]];
+                                set_layer_arr(l);
+                            }}>    <MoveUp size={16} /></div>
+                            <div className="layer_thumbnail_move_button" onClick={() => {
+                                set_layer_arr(_ => _!.filter((_, j) => j !== i))
+                                if (layer_arr.length - 1 <= current_layer) set_current_layer(_ => _ - 1);
+                            }}>     <X size={16} /></div>
+                            <div className="layer_thumbnail_move_button" onClick={() => {
+                                if (i + 1 === layer_arr.length) return;
+                                const l = [...layer_arr];
+                                if (i === current_layer) { set_current_layer(i + 1); }
+                                else if (i + 1 === current_layer) { set_current_layer(i); }
+                                [l[i], l[i + 1]] = [l[i + 1], l[i]];
+                                set_layer_arr(l);
+                            }}><MoveDown size={16} /></div>
                         </div>
                     </div>
                 ))}
