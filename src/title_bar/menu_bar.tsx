f549f97d4f7a25b4abe5a490f3e65cac8160ec80
bananahexagon <obsidian.bananafool@gmail.com>
2024-10-12 14:14:39 +0900

    #

--- src/title_bar/menu_bar.tsx
+++ src/title_bar/menu_bar.tsx
@@ -1,15 +1,16 @@
 import { invoke } from "@tauri-apps/api/core";
 import { Window } from "@tauri-apps/api/window";
 import React, { Dispatch, ReactNode, SetStateAction, useEffect, useRef, useState } from "react";
-import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { SetterOrUpdater, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { canvas_size_state, current_layer_state, is_loading_state, layer_arr_state, load_file, opening_file_path_state, user_data_state } from "../app";
 import { export_image, open_file, save_file_new, save_file_with_path, write_user_data } from "../file";
 import { is_modal_open_state, modal_contents_state, modal_size_state } from "../modal";
 import { Info } from "lucide-react";
 import { getTauriVersion } from "@tauri-apps/api/app";
-import { create_canvas, Option, PromiseWithResolvers, State } from "../common/utils";
+import { create_canvas, Option, PromiseWithResolvers, Result, State } from "../common/utils";
 import { file_save_state } from ".";
 import { undo_stack } from "../canvas_area/undo";
+import { Layer } from "../data";
 
 export const MenuBar = () => {
     const menu_bar_ref = useRef<HTMLDivElement>(null);
@@ -120,46 +121,40 @@ export const MenuBar = () => {
                     })
                     write_user_data({ user_data: user_data.unwrap() })
                 }} >開く</MenuContent>
-                <MenuContent on_click={async () => {
-                    set_selected(-1);
-                    const { promise, resolve } = PromiseWithResolvers<Option<number>>();
-                    let r = NaN;
-                    set_modal_open(true);
-                    set_modal_size({ w: 500, h: 200 });
-                    set_modal_contents([<div className="image_export_modal">
-                        <div className="image_export_modal_title">
-                            エクスポート
-                        </div>
-                        <div className="image_export_modal_canvas_size">
-                            <div className="image_export_modal_canvas_size_part">
-                                <div className="image_export_modal_canvas_size_part_title">倍率</div>
-                                <input className="image_export_modal_text_box" type="number" defaultValue={100} min={1}
-                                    onInput={e => {
-                                        r = Math.max(1, Number.parseInt(e.currentTarget.value!))
-                                        e.currentTarget.value = `${r}`;
-                                    }} />%
-                            </div>
-                        </div>
-                        <div className="image_export_modal_confirm" onClick={() => {
-                            resolve(Option.Some(r / 100));
-                            set_modal_open(false);
-                        }} >エクスポート</div>
-                    </div>]);
-                    document.addEventListener("modal_close", _ => {
-                        resolve(Option.None())
-                    }, { once: true });
-                    (await promise).on_some(r_raw => {
-                        const r = Number.isNaN(r_raw) ? 1 : r_raw;
-                        const [canvas, ctx] = create_canvas({ width: canvas_size.width * r, height: canvas_size.height * r });
-                        ctx.imageSmoothingEnabled = false;
-                        ctx.scale(r, r);
-                        layer_arr.forEach(l => { ctx.drawImage(l.body, 0, 0) });
+                <MenuContent on_click={project_export({
+                    set_selected,
+                    set_modal_open,
+                    set_modal_size,
+                    set_modal_contents,
+                    canvas_size,
+                    layer_arr,
+                    opening_file_path,
+                    canvas_handler: async ({ canvas, opening_file_path }) => {
                         export_image({
                             img: canvas,
                             project_name: opening_file_path.val_global().unwrap_or("新規プロジェクト").split("/").at(-1)?.split("\\").at(-1)!.replace(/^(.+)\..+$/, '$1'),
                         })
+                    }
+                })}>エクスポート</MenuContent>
+                <MenuContent on_click={
+                    project_export({
+                        set_selected,
+                        set_modal_open,
+                        set_modal_size,
+                        set_modal_contents,
+                        canvas_size,
+                        layer_arr,
+                        opening_file_path,
+                        canvas_handler: async ({ canvas}) => {
+                            const data_url = canvas.toDataURL('image/png');
+                            const blob = await (await fetch(data_url)).blob();
+                            const clipboard_item = new ClipboardItem({
+                                [blob.type]: blob
+                            });
+                            await Result.from_try_catch_async(async () => await navigator.clipboard.write([clipboard_item]));
+                        }
                     })
-                }}>エクスポート</MenuContent>
+                }>クリップボードにエクスポート</MenuContent>
             </MenuButton>
             <MenuButton label="ヘルプ" id="title_bar_menu_help_button" nth={1} selected={selected} set_selected={set_selected}>
                 <a href="https://bananahexagon.github.io/banahexel_site" target="_blank"><MenuContent on_click={() => {
@@ -215,6 +210,66 @@ export const MenuBar = () => {
             </MenuButton>
         </div >
     )
+}
+
+const project_export = ({
+    set_selected,
+    set_modal_open,
+    set_modal_size,
+    set_modal_contents,
+    canvas_size,
+    layer_arr,
+    opening_file_path,
+    canvas_handler
+}: project_export_argsT & {
+    canvas_handler: (args: project_export_argsT & { canvas: HTMLCanvasElement }) => Promise<void>;
+}) => async () => {
+    set_selected(-1);
+    const { promise, resolve } = PromiseWithResolvers<Option<number>>();
+    let r = NaN;
+    set_modal_open(true);
+    set_modal_size({ w: 500, h: 200 });
+    set_modal_contents([<div className="image_export_modal">
+        <div className="image_export_modal_title">
+            エクスポート
+        </div>
+        <div className="image_export_modal_canvas_size">
+            <div className="image_export_modal_canvas_size_part">
+                <div className="image_export_modal_canvas_size_part_title">倍率</div>
+                <input className="image_export_modal_text_box" type="number" defaultValue={100} min={1}
+                    onInput={e => {
+                        r = Math.max(1, Number.parseInt(e.currentTarget.value!))
+                        e.currentTarget.value = `${r}`;
+                    }} />%
+            </div>
+        </div>
+        <div className="image_export_modal_confirm" onClick={() => {
+            resolve(Option.Some(r / 100));
+            set_modal_open(false);
+        }} >エクスポート</div>
+    </div>]);
+    document.addEventListener("modal_close", _ => {
+        resolve(Option.None())
+    }, { once: true });
+    (await promise).on_some(r_raw => {
+        const r = Number.isNaN(r_raw) ? 1 : r_raw;
+        const [canvas, ctx] = create_canvas({ width: canvas_size.width * r, height: canvas_size.height * r });
+        ctx.imageSmoothingEnabled = false;
+        ctx.scale(r, r);
+        layer_arr.forEach(l => { ctx.drawImage(l.body, 0, 0) });
+        canvas_handler({ canvas, set_selected, set_modal_open, set_modal_size, set_modal_contents, canvas_size, layer_arr, opening_file_path })
+
+    })
+}
+
+type project_export_argsT = {
+    set_selected: SetterOrUpdater<number>
+    set_modal_open: SetterOrUpdater<boolean>,
+    set_modal_size: SetterOrUpdater<{ w: number, h: number }>,
+    set_modal_contents: SetterOrUpdater<string | React.ReactNode[]>,
+    canvas_size: { width: number, height: number },
+    layer_arr: Layer[],
+    opening_file_path: State<Option<string>>,
 }
 
 const MenuButton = (props: { label: string, id: string, nth: number, selected: number, set_selected: Dispatch<SetStateAction<number>>, children: ReactNode }) => {
